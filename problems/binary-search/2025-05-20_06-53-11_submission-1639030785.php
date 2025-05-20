cfb01d0d8063044f0fdcc2352682b6b4e98c729f
Yousef Dergham <egytech45189@gmail.com>
2025-05-20 06:53:11 +0000

    Solve: Binary Search (php) — submission 1639030785 @ 2025-05-20 06:53:11 UTC

---
+++ problems/binary-search/2025-05-20_06-53-11_submission-1639030785.php
@@ -0,0 +1,24 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function search($nums, $target) {
+        $start = 0;
+        $end = count($nums) - 1;
+        while($start<=$end){
+            $mid = (int) (($start + $end)/2);
+            if($nums[$mid]==$target){
+                return $mid;
+            }
+          if($nums[$mid]<$target){
+          $start = $mid +1;
+          }else{
+           $end = $mid -1;
+          }
+        }
+        return -1;
+    }
+}
