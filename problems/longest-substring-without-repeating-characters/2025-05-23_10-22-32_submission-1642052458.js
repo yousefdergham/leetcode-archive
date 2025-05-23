b87e81065f7814113579e0e126c455b082da5ca5
Yousef Dergham <egytech45189@gmail.com>
2025-05-23 10:22:32 +0000

    Solve: Longest Substring Without Repeating Characters (javascript) — submission 1642052458 @ 2025-05-23 10:22:32 UTC

---
+++ problems/longest-substring-without-repeating-characters/2025-05-23_10-22-32_submission-1642052458.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    if(s.length<3){
+        return s.length
+    }
+    let rightSet = new Set
+    let rightCount = 0
+    let leftSet = new Set
+    let breakPiont = s.length / 2
+    for (a in s) {
+        if (rightCount >= breakPiont) {
+            leftSet.add(s[a])
+        }else{
+        rightSet.add(s[a])
+        }
+        rightCount++
+    }
+   if(rightSet.size>leftSet.size){
+    return rightSet.size
+   }else{
+    return leftSet.size
+   }
+};
