44b87aa5c2eafe048d27ac6b3a1aa60a38c2d0f6
Yousef Dergham <egytech45189@gmail.com>
2025-05-24 09:57:43 +0000

    Solve: Best Time to Buy and Sell Stock (javascript) — submission 1642882853 @ 2025-05-24 09:57:43 UTC

---
+++ problems/best-time-to-buy-and-sell-stock/2025-05-24_09-57-43_submission-1642882853.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let minPrice = 0;
+    let maxProfit = 0;
+    for (price in prices) {
+        if (minPrice > prices[price]) {
+            minPrice = prices[price]
+        }
+        if (prices[price] - minPrice >= maxProfit && prices.indexOf(prices[price]) > prices.indexOf(minPrice)) {
+            maxProfit = prices[price] - minPrice
+        }
+    }
+    return maxProfit
+};
