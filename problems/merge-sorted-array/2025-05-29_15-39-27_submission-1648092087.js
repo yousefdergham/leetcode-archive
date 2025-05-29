8439f4b2a2f8da490e99958d60d4c5eba0e28a1f
Yousef Dergham <egytech45189@gmail.com>
2025-05-29 15:39:27 +0000

    Solve: Merge Sorted Array (javascript) — submission 1648092087 @ 2025-05-29 15:39:27 UTC

---
+++ problems/merge-sorted-array/2025-05-29_15-39-27_submission-1648092087.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    if(m>0){
+      num1Filterd = nums1.filter((x)=>x>0)
+    }
+    if(n>0){
+      num2Filterd = nums2.filter((x)=>x>0)
+    }
+    let arr = num1Filterd?num1Filterd.concat(num2Filterd).sort():num2Filterd.sort()
+    return arr
+};
