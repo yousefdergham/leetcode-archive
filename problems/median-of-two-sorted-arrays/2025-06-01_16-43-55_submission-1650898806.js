a34ccd06f7cd03671d1445d55135ea9a5677c25c
Yousef Dergham <egytech45189@gmail.com>
2025-06-01 16:43:55 +0000

    Solve: Median of Two Sorted Arrays (javascript) — submission 1650898806 @ 2025-06-01 16:43:55 UTC

---
+++ problems/median-of-two-sorted-arrays/2025-06-01_16-43-55_submission-1650898806.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let arr = nums1.concat(nums2).sort((x, y) => x - y)
+    let middle = Math.floor(arr.length/2)
+   if(arr.length % 2===0){
+   return (arr[middle] +arr[middle-1])/2
+   }else{
+  return arr[middle]
+   }
+};
