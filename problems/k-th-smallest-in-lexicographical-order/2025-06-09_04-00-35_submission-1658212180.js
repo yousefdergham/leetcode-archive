0f84763c8bffb8c7bb737b1dcc1385097ce93fe8
Yousef Dergham <egytech45189@gmail.com>
2025-06-09 04:00:35 +0000

    Solve: K-th Smallest in Lexicographical Order (javascript) — submission 1658212180 @ 2025-06-09 04:00:35 UTC

---
+++ problems/k-th-smallest-in-lexicographical-order/2025-06-09_04-00-35_submission-1658212180.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findKthNumber = function (n, k) {
+  let result = []
+  for (let i = 1; i <= n; i++) {
+    result.push(i)
+  }
+  result.sort()
+  return result[k-1]
+};
