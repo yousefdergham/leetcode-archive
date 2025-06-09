8f066a7c07484c3a21849b4c9f54bd7beacf0b55
Yousef Dergham <egytech45189@gmail.com>
2025-06-09 02:33:27 +0000

    Solve: Greatest Common Divisor of Strings (javascript) — submission 1658163623 @ 2025-06-09 02:33:27 UTC

---
+++ problems/greatest-common-divisor-of-strings/2025-06-09_02-33-27_submission-1658163623.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  if (str1.length == str2.length & str1[0] != str2[0]) {
+    return ""
+  }
+  let stringSet = new Set
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      if (str1[i] == str2[j]) {
+        stringSet.add(str1[i])
+      }
+
+    }
+
+  }
+  let result = Array.from(stringSet)
+  return result.join("")
+};
