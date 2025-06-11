826a9ae659af80575c0aca1a17a3de0932813c50
Yousef Dergham <egytech45189@gmail.com>
2025-06-11 03:25:54 +0000

    Solve: Palindrome Number (javascript) — submission 1660362972 @ 2025-06-11 03:25:54 UTC

---
+++ problems/palindrome-number/2025-06-11_03-25-54_submission-1660362972.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false
+  }
+  let arr = x.toString().split("")
+  if ((arr.length == 3 && arr[0] == arr[2])|| arr.length==1) {
+    return true
+  }
+  let mid = Math.ceil(arr.length / 2)
+
+  let right = arr.slice(0, mid)
+  let left = arr.slice(mid)
+  if (right.sort().join("") === left.sort().join("")) {
+    return true
+  }else{
+    return false
+  }
+};
