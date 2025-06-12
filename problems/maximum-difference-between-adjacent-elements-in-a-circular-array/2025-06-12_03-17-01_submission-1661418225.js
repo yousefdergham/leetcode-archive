b0e2ebf8332a4fe5a9b33f5df0aba7e1039c87e6
Yousef Dergham <egytech45189@gmail.com>
2025-06-12 03:17:01 +0000

    Solve: Maximum Difference Between Adjacent Elements in a Circular Array (javascript) — submission 1661418225 @ 2025-06-12 03:17:01 UTC

---
+++ problems/maximum-difference-between-adjacent-elements-in-a-circular-array/2025-06-12_03-17-01_submission-1661418225.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAdjacentDistance = function (nums) {
+  let absArr = nums.map(x => Math.abs(x)).sort((a, b) => a - b)
+  let f = absArr[0]
+  let s = absArr.filter(x => f < x).sort((a, b) => a + b)
+  return s[s.length - 1] - f
+};
