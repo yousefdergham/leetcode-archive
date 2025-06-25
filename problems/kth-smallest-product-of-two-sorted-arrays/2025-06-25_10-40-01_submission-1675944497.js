c0b302c067c029a3fa3a4eb56f9f80a48daf0122
Yousef Dergham <egytech45189@gmail.com>
2025-06-25 10:40:01 +0000

    Solve: Kth Smallest Product of Two Sorted Arrays (javascript) — submission 1675944497 @ 2025-06-25 10:40:01 UTC

---
+++ problems/kth-smallest-product-of-two-sorted-arrays/2025-06-25_10-40-01_submission-1675944497.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallestProduct = function (nums1, nums2, k) {
+  let result = []
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      result.push(nums1[i] * nums2[j])
+    }
+    if (i == k + 2) {
+      break;
+    }
+  }
+  result.sort((a, b) => a - b)
+
+  return result[k - 1]
+};
