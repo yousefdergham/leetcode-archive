08c17d2ac3161d6e025244d0bbddfb62827f0df6
Yousef Dergham <egytech45189@gmail.com>
2025-06-30 13:02:21 +0000

    Solve: Longest Harmonious Subsequence (javascript) — submission 1681461253 @ 2025-06-30 13:02:21 UTC

---
+++ problems/longest-harmonious-subsequence/2025-06-30_13-02-21_submission-1681461253.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function (nums) {
+  const subsequences = nums.reduce(
+    (subs, num) =>
+      subs.concat(subs.map(seq => [...seq, num])),
+    [[]]
+  ).filter(seq => seq.length > 0).sort((a, b) => a.length - b.length).reverse()
+  for (let i = 0; i < subsequences.length; i++) {
+    let tmp = subsequences[i].sort((a, b) => a - b)
+    console.log(tmp)
+    if (tmp[tmp.length - 1] - tmp[0] == 1) {
+      return tmp.length
+    }
+  }
+  return 0
+};
