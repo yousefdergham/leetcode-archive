0e5ae2434262637d42556a061a9056676ae5f281
Yousef Dergham <egytech45189@gmail.com>
2025-07-03 03:00:44 +0000

    Solve: Find the K-th Character in String Game I (javascript) — submission 1684495647 @ 2025-07-03 03:00:44 UTC

---
+++ problems/find-the-k-th-character-in-string-game-i/2025-07-03_03-00-44_submission-1684495647.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} k
+ * @return {character}
+ */
+var kthCharacter = function (k) {
+    let word = "a"
+    if (k == 1) {
+        return word
+    }
+    let steps = 0
+    while (word.length < k) {
+        let tmp = ""
+        for (let i = 0; i < word.length; i++) {
+            let newChar = word[i].charCodeAt(0) + 1
+            tmp += String.fromCharCode(newChar)
+        }
+        word = word + tmp
+        steps++
+    }
+
+    return String.fromCharCode((steps - 2) + 97)
+};
