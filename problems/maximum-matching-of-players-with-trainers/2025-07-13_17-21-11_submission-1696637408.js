ea09fc33f67615e16a8402d7e01cb0b76798ac69
Yousef Dergham <egytech45189@gmail.com>
2025-07-13 17:21:11 +0000

    Solve: Maximum Matching of Players With Trainers (javascript) — submission 1696637408 @ 2025-07-13 17:21:11 UTC

---
+++ problems/maximum-matching-of-players-with-trainers/2025-07-13_17-21-11_submission-1696637408.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} players
+ * @param {number[]} trainers
+ * @return {number}
+ */
+function matchPlayersAndTrainers(players, trainers) {
+  players.sort((a, b) => a - b);
+  trainers.sort((a, b) => a - b);
+
+  let i = 0, j = 0, matches = 0;
+  while (i < players.length && j < trainers.length) {
+    if (players[i] <= trainers[j]) {
+      ++matches;
+      ++i;
+    }
+    ++j;
+  }
+  return matches;
+}
