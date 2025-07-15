6f3d44bba7f8a620d2be720bdad682680380ddb9
Yousef Dergham <egytech45189@gmail.com>
2025-07-15 07:51:30 +0000

    Solve: Search Insert Position (javascript) — submission 1698506320 @ 2025-07-15 07:51:30 UTC

---
+++ problems/search-insert-position/2025-07-15_07-51-30_submission-1698506320.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let start = 0
+  let end = nums.length - 1
+  let mid
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2)
+    if (nums[mid] == target) {
+      return mid
+    }
+    if (nums[mid] < target) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+
+
+  }
+  if(target == 0){
+    return 0
+  }else{
+
+  return mid + 1
+  }
+};
