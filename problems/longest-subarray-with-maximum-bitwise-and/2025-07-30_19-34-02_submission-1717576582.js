62b3c573cde2a5155c06108dade58cd18a619412
Yousef Dergham <egytech45189@gmail.com>
2025-07-30 19:34:02 +0000

    Solve: Longest Subarray With Maximum Bitwise AND (javascript) — submission 1717576582 @ 2025-07-30 19:34:02 UTC

---
+++ problems/longest-subarray-with-maximum-bitwise-and/2025-07-30_19-34-02_submission-1717576582.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestSubarray = nums => {
+	const maxVal = Math.max(...nums)
+	let maxStreak = 0,
+		streak = 0
+	for (let i = 0; i < nums.length; i++) {
+		nums[i] === maxVal
+			? (streak++, streak > maxStreak && (maxStreak = streak))
+			: streak = 0
+	}
+	return maxStreak
+}
