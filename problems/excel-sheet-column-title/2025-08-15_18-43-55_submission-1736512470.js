f4397f756afbff73280ef157454793368f8b27ad
Yousef Dergham <egytech45189@gmail.com>
2025-08-15 18:43:55 +0000

    Solve: Excel Sheet Column Title (javascript) — submission 1736512470 @ 2025-08-15 18:43:55 UTC

---
+++ problems/excel-sheet-column-title/2025-08-15_18-43-55_submission-1736512470.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function (columnNumber) {
+  if (columnNumber <= 26) {
+    return String.fromCharCode(96 + columnNumber).toUpperCase()
+  } else {
+    let result = '';
+    while (columnNumber > 0) {
+      let remainder = columnNumber % 26 || 26;
+      result = String.fromCharCode(64 + remainder) + result;
+      columnNumber = Math.floor((columnNumber - remainder) / 26);
+    }
+    return result;
+  }
+};
