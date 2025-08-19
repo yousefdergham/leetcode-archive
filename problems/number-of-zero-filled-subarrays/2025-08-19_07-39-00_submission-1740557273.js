d0c29ad24fe59c16531bca999161d01294c40d46
Yousef Dergham <egytech45189@gmail.com>
2025-08-19 07:39:00 +0000

    Solve: Number of Zero-Filled Subarrays (javascript) — submission 1740557273 @ 2025-08-19 07:39:00 UTC

---
+++ problems/number-of-zero-filled-subarrays/2025-08-19_07-39-00_submission-1740557273.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const zeroFilledSubarray = nums => {
+    let ans = 0, numSub = 0;
+    for (const num of nums) {
+        if (num === 0) {
+            numSub++;
+        } else {
+            numSub = 0;
+        }
+        ans += numSub;
+    }
+    return ans;
+};
